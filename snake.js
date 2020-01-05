99b8f233011938c999f47ee6cac17d6743498de7
Alex Konichek <Konichekalex@gmail.com>
2020-01-05 21:36:25 +0200

    change touch events

--- snake.js
+++ snake.js
@@ -74,7 +74,7 @@ function direction(event){
 document.addEventListener('touchstart', function(event) {
     event.preventDefault();
     event.stopPropagation();
-    ctx.fillText("А НУ НЕ ЖМИ НА МЕНЯ!",5*box,6*box);
+    ctx.fillText("НЕ ЖМИ НА МЕНЯ!",3*box,6*box);
     }, false);
 
 document.addEventListener('touchmove', function(event) {
