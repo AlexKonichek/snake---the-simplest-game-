157fac5224bd9e6f46ad612caebc8779cb7bcf81
Alex Konichek <Konichekalex@gmail.com>
2020-01-15 10:15:28 +0200

    little improvement and upgrade naming

--- snake.js
+++ snake.js
@@ -1,7 +1,7 @@
 const cvs = document.getElementById("snake");
 const ctx = cvs.getContext("2d");
 
-// create the unit
+// create the box (like a big pixel)
 const box = 32;
 
 // load images
@@ -28,7 +28,7 @@ right.src = "sound/right.mp3";
 left.src = "sound/left.mp3";
 down.src = "sound/down.mp3";
 
-// create the snake
+// create the snake array
 
 let snake = [];
 
@@ -37,41 +37,43 @@ snake[0] = {
     y : 10 * box
 };
 
-// create the food
+// create the food with random coordinates
 
 let food = {
     x : Math.floor(Math.random()*17+1) * box,
     y : Math.floor(Math.random()*15+3) * box
 }
 
-// create the score var
+// create the score
 
 let score = 0;
 
-//control the snake
 
-let d;
+//control the snake
+let dir;
 
 document.addEventListener("keydown",direction);
 
 function direction(event){
     let key = event.keyCode;
-    if( key == 37 && d != "RIGHT"){
+    if( key == 37 && dir != "RIGHT"){
         console.log(key)
         left.play();
-        d = "LEFT";
-    }else if(key == 38 && d != "DOWN"){
-        d = "UP";
+        dir = "LEFT";
+    }else if(key == 38 && dir != "DOWN"){
+        dir = "UP";
         up.play();
-    }else if(key == 39 && d != "LEFT"){
-        d = "RIGHT";
+    }else if(key == 39 && dir != "LEFT"){
+        dir = "RIGHT";
         right.play();
-    }else if(key == 40 && d != "UP"){
-        d = "DOWN";
+    }else if(key == 40 && dir != "UP"){
+        dir = "DOWN";
         down.play();
     }
 }
-document.addEventListener('touchstart', function(event) {
+
+// touch events for mobile version
+/* document.addEventListener('touchstart', function(event) {
     event.preventDefault();
     event.stopPropagation();
     ctx.fillText("НЕ ЖМИ НА МЕНЯ!",3*box,6*box);
@@ -80,15 +82,17 @@ document.addEventListener('touchstart', function(event) {
 document.addEventListener('touchmove', function(event) {
         event.preventDefault();
         event.stopPropagation();
-        /* Здесь ваш код обработки события*/
+       
         }, false);
 
 document.addEventListener('touchend', function(event) {
             event.preventDefault();
             event.stopPropagation();
             ctx.fillText("",5*box,6*box);
-            }, false);
-// cheack collision function
+            }, false); */
+
+
+// check collision 
 function collision(head,array){
     for(let i = 0; i < array.length; i++){
         if(head.x == array[i].x && head.y == array[i].y){
@@ -119,10 +123,10 @@ function draw(){
     let snakeY = snake[0].y;
     
     // which direction
-    if( d == "LEFT") snakeX -= box;
-    if( d == "UP") snakeY -= box;
-    if( d == "RIGHT") snakeX += box;
-    if( d == "DOWN") snakeY += box;
+    if( dir == "LEFT") snakeX -= box;
+    if( dir == "UP") snakeY -= box;
+    if( dir == "RIGHT") snakeX += box;
+    if( dir == "DOWN") snakeY += box;
     
     // if the snake eats the food
     if(snakeX == food.x && snakeY == food.y){
@@ -161,7 +165,7 @@ function draw(){
     ctx.fillText(score,2*box,1.6*box);
 }
 
-// call draw function every 100 ms
+// call draw function every 100 ms (instead RequestAnimFrame)
 
 let game = setInterval(draw,300);
 
